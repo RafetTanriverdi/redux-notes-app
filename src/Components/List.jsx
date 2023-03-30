4f7132069aa9ddb5bfbd65a7d17f5a3d1eab1b3a
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2023-03-30 12:15:04 +0300

    project is about to end

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,30 +1,23 @@
 import React, { useState } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
-import { onClickNote } from "../Redux/note/noteSlice";
-import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
-
 import ModalComponent from "./ModalComponent";
+import { onClickNote } from "../Redux/note/noteSlice";
 
 function List() {
   const [isOpen, setIsOpen] = useState(false);
   const notes = useSelector((state) => state.notes.notes);
   const filtered = useSelector((state) => state.notes.filtered);
   const dispatch = useDispatch();
-
-
-
   const filteredNotes = notes.filter((item) =>
     item.note.toLowerCase().includes(filtered.toLowerCase())
   );
-  const handleClick = (item) => {
+  const handelClick = (item) => {
     dispatch(onClickNote(item));
     setIsOpen(!isOpen);
   };
-
   return (
     <div>
-
       <Box
         display="flex"
         justifyContent="center"
@@ -38,20 +31,20 @@ function List() {
             m="1"
             p="1"
             bg={item.color}
-            position="relative"
+           
             borderRadius="16"
             rounded="xl"
             boxShadow="xl"
             overflow="hidden"
             textOverflow="ellipsis"
-            onClick={() => handleClick(item)}
+            onClick={() => handelClick(item)}
             display="flex"
             justifyContent="center"
             alignItems="center"
             textAlign="center"
           >
             <Text
-
+           
               color={item.color === "blue" ? "white" : "gray.700"}
               textShadow="0 0 1px #4A5568"
               p={1}
@@ -60,20 +53,10 @@ function List() {
             >
               {item.note}
             </Text>
-
-            <div style={{ position: "absolute", bottom: "15px", right: "15px", left: "15px", display: "flex", justifyContent: "space-between", cursor: "pointer" }}>
-
-              <button onClick={handleClick} >
-                <EditIcon />
-              </button>
-              <button>
-                <DeleteIcon />
-              </button>
-            </div>
           </Box>
         ))}
       </Box>
-      {isOpen && <ModalComponent setIsOpen={setIsOpen} isOpen={isOpen} />}
+      {isOpen && <ModalComponent isOpen={isOpen} setIsOpen={setIsOpen} />}
     </div>
   );
 }
--- src/Components/ModalComponent.jsx
+++ src/Components/ModalComponent.jsx
@@ -1,68 +1,95 @@
-import React, { useState } from 'react'
+import { Textarea, useToast } from "@chakra-ui/react";
+import { Button, Modal, Popconfirm } from "antd";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { edit } from "../Redux/note/noteSlice";
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button, Textarea, useToast
-} from '@chakra-ui/react'
-
-function ModalComponent({ setIsOpen, isOpen }) {
-    const toast = useToast();
-    const dispatch = useDispatch();
-    const item = useSelector((state) => state.notes.currentNote);
-    const [editNote, setEditNote] = useState(item.note);
-
-    const saveSubmit = () => {
-        dispatch(edit(editNote));
-        setIsOpen(!isOpen);
-        toast({
-            description: "Note saved.",
-            status: "success",
-            duration: 3000,
-            isClosable: true,
-        });
-    };
-
-    const onClose = () => {
-        setIsOpen(false)
-    }
-    return (
-        <Modal isOpen={isOpen} onClose={onClose} >
-            <ModalOverlay />
-            <ModalContent>
-                <ModalCloseButton />
-                <ModalBody>
-
-                    <Textarea
-                        value={editNote}
-                        onChange={(e) => setEditNote(e.target.value)}
-                        fontWeight="300"
-                        w="100%"
-                        h="160px"
-                        placeholder="Enter your note here"
-                        resize="none"
-                        focusBorderColor="none"
-                        spellCheck="false"
-                    />
+import { edit, deleteNote } from "../Redux/note/noteSlice";
 
 
 
-                </ModalBody>
+const ModalComponent = ({ setIsOpen, isOpen }) => {
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const dispatch = useDispatch();
+  const item = useSelector((state) => state.notes.currentNote);
+  const [editNote, setEditNote] = useState(item.note);
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setIsOpen(false);
+    }, 3000);
+  };
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
+  const saveSubmit = () => {
+    dispatch(edit(editNote));
+    setIsOpen(!isOpen);
+    toast({
+      description: "Note saved.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
 
-                <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                        Close
-                    </Button>
-                    <Button variant='ghost'  type='submit' onClick={saveSubmit}>Save</Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
-    )
-}
+  const deleteSubmit = () => {
+    dispatch(deleteNote());
+    setIsOpen(!isOpen);
+    toast({
+      title: "Note deleted",
+      duration: 3000,
+      status: "warning",
+      isClosable: true,
+    });
+  };
+  return (
+    <>
+      <Modal
+        open={isOpen}
+        title="Note Details"
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Return
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            loading={loading}
+            onClick={saveSubmit}
+          >
+            Save
+          </Button>,
 
-export default ModalComponent
+          <Popconfirm
+            key="delete"
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            onConfirm={deleteSubmit}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button danger type="primary" loading={loading}>
+              Delete
+            </Button>
+          </Popconfirm>,
+        ]}
+      >
+        <Textarea
+          value={editNote}
+          onChange={(e) => setEditNote(e.target.value)}
+          fontWeight="300"
+          w="100%"
+          h="160px"
+          placeholder="Enter your note here"
+          resize="none"
+          focusBorderColor="none"
+          spellCheck="false"
+        />
+      </Modal>
+    </>
+  );
+};
+export default ModalComponent;
